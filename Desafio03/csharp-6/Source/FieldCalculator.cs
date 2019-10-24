029e205390995e67676f4333a8ad5fd581e9d18a
romanoConto <romanodeconto@gmail.com>
2019-10-23 22:17:38 -0300

    m

--- Desafio03/csharp-6/Source/FieldCalculator.cs
+++ Desafio03/csharp-6/Source/FieldCalculator.cs
@@ -1,6 +1,62 @@
 namespace Codenation.Challenge
 {
-    public class FieldCalculator: ICalculateField
+    public class FieldCalculator : ICalculateField
     {
+        [Add]
+        private decimal salary;
+
+        [Add]
+        private decimal dsr;
+
+        [Subtract]
+        private decimal shop;
+
+        [Subtract]
+        private decimal study;
+
+        public decimal Addition(object obj)
+        {
+            decimal value = new decimal(0);
+
+            var type = obj.GetType();
+
+            foreach(var prop in type.GetProperties())
+            {
+                var atribute = prop.GetCustomAttributes().First();
+                if(prop.Value.type() != decimal.type() && atribute != null && atribute = AddAttribute)
+                {
+                    value += prop.GetValue();
+                }
+            }
+
+            return value;
+        }
+
+        public decimal Subtraction(object obj)
+        {
+            decimal value = new decimal(0);
+
+            var type = obj.GetType();
+
+            foreach (var prop in type.GetProperties())
+            {
+                var atribute = prop.GetCustomAttributes().First();
+                if (prop.Value.type() != decimal.type() && atribute != null && atribute = SubtractAttribute)
+                {
+                    value -= prop.GetValue();
+                }
+            }
+
+            return value;
+        }
+
+        public decimal Total(object obj)
+        {
+            decimal value = new decimal(0);
+
+            value += (Addition(obj) - Subtraction(obj));
+
+            return value;
+        }
     }
 }
